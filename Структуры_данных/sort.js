4dff7e334f68e02ac3b6a0a3e94c7cbc78998aff
NadyaGus <ftotkexit@gmail.com>
2024-10-18 13:02:29 +0300

    feat: add solution for sort

---
+++ Структуры_данных/sort.js
@@ -0,0 +1,49 @@
+const food = ["Apple", "Melon", "Banana", "Yogurt", "Orange", "Stawberry"];
+
+const players = [
+    {
+        id: 1,
+        name: 'Cristiano',
+        surname: 'Ronaldo',
+        club: 'Juventus',
+    },
+    {
+        id: 2,
+        name: 'Lionel',
+        surname: 'Messi',
+        club: 'Barcelona',
+    },
+    {
+        id: 3,
+        name: 'Karim',
+        surname: 'Benzema',
+        club: 'Real Madrid',
+    },
+    {
+        id: 4,
+        name: 'Maxi',
+        surname: 'Gomez',
+        club: 'Valencia',
+    },
+    {
+        id: 5,
+        name: 'Quincy',
+        surname: 'Promes',
+        club: 'Spartak',
+    },
+];
+
+const sortFunction = (arr, key) => {
+    return arr.sort((a, b) => {
+        if (a[key] < b[key]) {
+            return -1;
+        }
+        if (a[key] > b[key]) {
+            return 1;
+        }
+        return 0;
+    })
+};
+
+console.log(sortFunction(food, '0'));
+console.log(sortFunction(players, 'surname'));
